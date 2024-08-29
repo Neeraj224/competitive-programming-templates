7556ccba997d5535da6c3ac1c0b56a629b0ba778
Neeraj Chandel <34812602+Neeraj224@users.noreply.github.com>
2024-08-29 14:45:22 -0400

    Update graphs.py - adding BFS cycle detection...

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -54,6 +54,58 @@ class Graph:
                         self.BFSqueue.append(neighbor)
         
         return self.BFSTraversal
+    
+    def hasCycle(self, node):
+        visited = set()
+        
+        """
+            detect() is a modified BFS.
+            this code is mainly for cycle detection in undirected graphs!
+            for directed graphs, DFS is better.
+            But in the longer run, the more the nodes, the worse recursive DFS will perform.
+            So we will try to implement a better iterative DFS
+        """
+        def detect(node, visited):
+            visited.add(node)
+            
+            queue = []
+            # in our queue, instead of just storing the current node, we will also 
+            # store the parent of the current node, sincde we wanna detect a cycle.
+            # it will help to check in an indirected graph, since the parent will also be
+            # in the children's neighbors' lists.
+            # at the very beginning for our source node, we will add the parent as -1
+            queue.append((node, -1))
+            
+            while queue:
+                # get the current node and its parent
+                current, parent = queue.pop(0)
+                
+                if self.adjacency_list[current] is not None:
+                    for neighbor in self.adjacency_list[current]:
+                        if neighbor not in visited:
+                            # if it wasnt visited, add it to the visited set:
+                            visited.add(neighbor)
+                            # then add the neighbor and its parent (i.e. current) to the queue
+                            # for further traversal:
+                            queue.append((neighbor, current))
+                        # if the neighbor was in visited, and if that neighbor is not the parent,
+                        # that means we have found a cycle!
+                        elif neighbor != parent:
+                            return True
+                        # if that neighbor is the parent, then we can just keep processing the queue
+
+            return False
+
+        # now we have to run detect() through all the nodes in the
+        # adjacency list - unless they're visited (obviously)
+        # because the graph can also have other disjoint components
+        # that might or might not have cycles:
+        for i in range(len(self.adjacency_list)):
+            if i not in visited:
+                if detect(i, visited):
+                    return True
+        
+        return False
             
     def printGraph(self):
         for node in range(len(self.adjacencyList)):
